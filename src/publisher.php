4f04b7ba9701cbef89abd8b6dda4fd2a8463e55c
Prashant <prashant.sonawane@esds.co.in>
2018-11-29 19:19:15 +0530

    a

--- src/publisher.php
+++ src/publisher.php
@@ -1,8 +1,10 @@
 <?php
 //error_reporting(E_ALL);
 
-require_once('vendor/autoload.php');
+//require_once('vendor/autoload.php');
 //require dirname(__DIR__).'/vendor/autoload.php';
+
+require_once __DIR__ . '../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
